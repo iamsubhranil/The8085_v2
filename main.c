c6293f719665903084bab2fcf229cf56d7ed0d6c
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-07-21 00:58:39 +0530

    new: Added reading from file faciality

--- main.c
+++ main.c
@@ -5,7 +5,36 @@
 #include "bytecode.h"
 #include "compiler.h"
 
-int main(){
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    } 
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+int main(int argc, char *argv[]){
     Machine m;
     m.pc = m.sp = 0;
     for(u8 i = 0;i < 8; i++){
@@ -16,12 +45,12 @@ int main(){
     u16 pointer = 0;
     u16 *p = &pointer;
 
-    const char *source =    "mvi h, 10h\n"
-                            "mvi l, 10h\n"
-                            "mvi m, 19h\n"
-                            "ldax h\n"
-                            "cpi 19h\n"
-                            "hlt\n";
+    if(argc == 1){
+        perr("Usage : %s file_name\n", argv[0]);
+        return 1;
+    }
+
+    char *source = readFile(argv[1]);
 
     pinfo("Source");
     pinfo("======\n");
@@ -44,4 +73,7 @@ int main(){
     pinfo("Running chunk");
     pinfo("=============\n");
     run(&m, &memory[0]);
+
+    printf("\n");
+    return 0;
 }
